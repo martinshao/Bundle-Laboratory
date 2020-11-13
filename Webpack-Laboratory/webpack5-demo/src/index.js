a9bf6d43a7f20f2290ccd657898907ba88488f20
Martin-Shao <shaogucheng@hotmail.com>
2020-11-13 23:53:02 +0800

    🎉 未引入lodash之前的入口文件

---
+++ Webpack-Laboratory/webpack5-demo/src/index.js
@@ -0,0 +1,9 @@
+function component() {
+  const element = document.createElement('div');
+  // lodash （目前通过一个 script 引入）对于执行这一行是必须的
+  element.innerHTML = _.join(['Hello', 'webpack'], '');
+
+  return element
+}
+
+document.body.appendChild(component())
