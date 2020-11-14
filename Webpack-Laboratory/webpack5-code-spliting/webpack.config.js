596a2b0f6caf24fafd37321fbfca2c04f197aa24
Martin-Shao <shaogucheng@hotmail.com>
2020-11-14 21:44:11 +0800

    🚧 配置 dependOn option 选项，这样可以在多个 chunk 之间共享模块。

--- Webpack-Laboratory/webpack5-code-spliting/webpack.config.js
+++ Webpack-Laboratory/webpack5-code-spliting/webpack.config.js
@@ -1,13 +1,14 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
   mode: 'development',
   entry: {
-    index: './src/index.js',
-    another: './src/another-module.js',
+    index: {import: './src/index.js', dependOn: 'shared'},
+    another: {import: './src/another-module.js', dependOn: 'shared'},
+    shared: 'lodash',
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
-}
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
