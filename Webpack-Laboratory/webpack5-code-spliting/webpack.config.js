4189364375f113d1a6be40e5437a11d31e912025
Martin-Shao <shaogucheng@hotmail.com>
2020-11-14 22:10:20 +0800

    ✨ SplitChunksPlugin 插件可以将公共的依赖模块提取到已有的入口 chunk 中

--- Webpack-Laboratory/webpack5-code-spliting/webpack.config.js
+++ Webpack-Laboratory/webpack5-code-spliting/webpack.config.js
@@ -3,12 +3,16 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: {
-    index: {import: './src/index.js', dependOn: 'shared'},
-    another: {import: './src/another-module.js', dependOn: 'shared'},
-    shared: 'lodash',
+    index: './src/index.js',
+    another: './src/another-module.js',
   },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
   },
 };
